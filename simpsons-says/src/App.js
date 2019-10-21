e212f439bf2cac0b027f1a0a25b9626bce352aeb
theJOHNwatt <j.watt10@gmail.com>
2019-10-21 12:44:22 -0700

    added login and register components and code. waiting for information for authentication

--- simpsons-says/src/App.js
+++ simpsons-says/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from './components/Login'
+import Register from './components/Register'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Route exact path='/' component={Login} />
+        <Route path ="/register" component={Register} /> 
+      </Router>
     </div>
   );
 }
--- simpsons-says/src/components/Login.js
+++ simpsons-says/src/components/Login.js
@@ -1,1 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Login = (props) => {
+  const [loginCredentials, setLoginCredentials] = useState({
+    email: '',
+    password: ''
+  });
+
+//   const handleChange = (e) => {
+//     setLoginCredentials({
+//       ...loginCredentials,
+//       [e.target.name]: e.target.value
+//     })
+//     console.log('login creds: ', loginCredentials)
+//   }
+
+
+
+  return (
+    <div className="login-screen">
+
+      <div className="login-form-container">
+
+        <h2 className="login-form-title">Simpsons Says</h2>
+        
+
+        {/* add on submit for form */}
+        <form  className="login-form">
+          <input 
+        //   onChange={handleChange}
+          className="input-email"
+          name="email"
+          type="email"
+          placeholder="Email"
+          value = {loginCredentials.email}
+          required
+          />
+          <input 
+        //   onChange={handleChange}
+          className="input-password"
+          name="password"
+          type="password"
+          placeholder="Password"
+          value = {loginCredentials.password}
+          required
+          />
+          <button>Sign In</button>
+        </form>
+
+        <div className="register-description">
+          <p>Don't have an account? Sign Up <Link to='/register'>Here</Link></p>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Login;
